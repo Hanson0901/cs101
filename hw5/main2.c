5296b26ec64266b8e0db278d49557e58103e1772
Hanson Yang(Mikuma hiroki) <101704292+Hanson0901@users.noreply.github.com>
2023-12-20 21:57:57 +0800

    Create main2.c

---
+++ hw5/main2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+int i=1,sign=1;
+double t=1,pi=0;
+while(fabs(t)>=1e-8){
+  pi=pi+t;
+  sign=-sign;
+  i=i+2;
+  t=1.0*sign/i;
+  }
+  printf("%.5f",4*pi);
+  return 0;
+}
